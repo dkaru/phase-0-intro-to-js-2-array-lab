2e2ac06eba20a053d145b8e7a477d3d87261cd30
Moringa School <dwangui799@gmail.com>
2022-09-27 15:25:15 +0300

    complited assignment

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
 // Write your solution here!
+var cats =['Milo', 'Otis', 'Garfield ', ]
+function destractivelyAppendCat (name) {
+cats.push (name)
+}
+function destructivelyprependCat (name) {
+    cats . unshift (nane)
+}
+function destructivelyRemoveLAstCat() {
+cats . pop ()
+return cats;
+}
+function destructivelyRemoveFirstCat () {
+cats .shift ()
+return cats;
+}
+function appendCat(name){
+    var name_1 =[... cats,name];
+return name_1;
+}
+ function prependCat (name) {
+  var name_2 =[nane, ...cats]  
+  return name_2
+ }
+function removeLastcat() {
+ var MiniArrayOfpets = cats .slice (0, cats . length-1)  
+ return  MiniArrayOfpets;
+}
+function removeFirstcat() {
+    var NewArrayOfCats = cats . slice(1) 
+    return NewArrayOfCats
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
